84a2094427e2ecd2a024c899454f63b9b04c80ea
Roger Chui <roger.chui@csbio-l.csr.uky.edu>
2013-03-16 02:52:52 -0400

    Placeholders, improved threading

--- run.pl
+++ run.pl
@@ -11,7 +11,8 @@ use threads::shared;
 
 use Getopt::Long qw(:config gnu_getopt);
 
-our ($threads, $verbose, $instance);
+our $maxthreads : shared;
+our ($verbose, $instance);
 
 BEGIN {
 # get instance for use
@@ -34,45 +35,60 @@ EOF
 
 	my $help;
 	GetOptions(
-			't|threads=i' =>\$threads,
+			't|threads=i' =>\$maxthreads,
 			'h|help' => \$help,
 			'v|verbose' => \$verbose,
 		  ) or usage(1);
 
 	$help and usage(0);
 	@ARGV == 1 or usage(2);
-	$threads or $threads = 4;
+	$maxthreads or $maxthreads = 4;
 	$instance = shift;
 }
-
 ### Include libraries from the target instance
 use lib "/home/fpd/deployed/$instance/CGI/tools/";
-use FPD::Config;
-use FPD::App::Gbrowse;
 
-### Connect to the database using settings in the target instance configuration
-my $gbdbh;
-FPD::Config::connection("gbrowse")->use_database_handle(sub { $gbdbh = shift; });
-
-### Set up the variables
-# create queue of objects to be worked on
+### Set up the global variables
+# queue of objects to be worked on
 my $workqueue = Thread::Queue->new();
-
-# an array of the threads. we use $THREAD_LIMIT threads.
+# an array of the threads. we use $maxthreads threads.
 my @threads;
-
-# a kind-of semaphore to keep file writes atomic
+# kind-of mutex to keep file writes atomic
 my $filelock : shared;
 
-### Get the features to process
-{ 
-	# get the features from the gbrowse database
-	my $sth = $gbdbh->prepare("SELECT gclass, gname FROM fgroup WHERE gclass LIKE 'gff3%' LIMIT 100;");
+# run the queue filler
+threads->new(\&queuefiller)->join;
+
+# start the worker threads
+for (1 .. $maxthreads) {
+	my $t = threads->new(\&worker);
+	push (@threads,$t);
+	print "Thread " . $t->tid() ." created\n" if $verbose;
+}
+
+# now we twiddle our thumbs and wait for threads to finish working
+$_->join() for @threads;
+
+### done!
+exit(0);
+
+##### subs below this point #####
+
+### queuefiller: Get the features to process
+# get the features from the gbrowse database and give the workers tasks
+sub queuefiller {
+	require FPD::Config;
+	
+	# Connect to the database using settings in the target instance configuration
+	my $gbdbh;
+	FPD::Config::connection("gbrowse")->use_database_handle(sub { $gbdbh = shift; });
+
+	my $sth = $gbdbh->prepare("SELECT gclass, gname FROM fgroup WHERE gclass LIKE 'gff3%' LIMIT 1000;");
 	$sth->execute();
 
 	while (my $row = $sth->fetchrow_hashref()) {
 		# for each feature, put it in the queue
-		my %feature : shared;
+		my %feature:shared;
 		$feature{name} = $row->{gname};
 		# if feature has both type and track number (?) then split it
 		if ($row->{gclass} =~ /(\w+):(\d+)/) {
@@ -87,46 +103,53 @@ my $filelock : shared;
 		# enqueue the feature
 		$workqueue->enqueue(\%feature);
 	}
+
+	# enqueue $maxthreads number of EOQ signals
+	for (1 .. $maxthreads) {
+		my %f:shared;
+		$f{endofqueue} = 1;
+		$workqueue->enqueue(\%f);
+	}
+
+	# done filling, return
+	return;
 }
 
-# start the worker threads
-for (1 .. $threads) {
-	my $t = threads->new(\&worker);
-	push (@threads,$t);
-	print "Thread " . $t->tid() ." created\n" if $verbose;
-}
 
-# now we twiddle thumbs and wait for threads to finish working
-$_->join() for @threads;
-
+### worker: decode each feature into gff3
+# for each feature queued, determine what type it is and run the appropriate conversion
 sub worker {
 	require FPD::Config;
 	require FPD::GFF3;
 
+	# our own DB handle
 	my $ppdbh;
 	FPD::Config::connection("pp")->use_database_handle(sub { $ppdbh = shift; });
 
-	while (my $feature = $workqueue->dequeue_nb()) {
-		return unless $feature; # end of queue, rejoin
+	while (my $feature = $workqueue->dequeue()) {
+		return if $feature->{endofqueue}; # end of queue, rejoin
 		
 		my $gff;
-		# TODO: for each feature type
-
 		### GFF3 features
 		if ($feature->{'type'} eq "gff3") {
 			if ($feature->{name} =~ /^\w+.(\d+)( .*)?$/) {
-				my $sth = $ppdbh->prepare("select gff3.*, geneid from gff3 left join gff3gene on gffid = gff3.id where id=?")->execute($1);
+				# we could just use GFF3->from_db but it's not thread safe :(
+				my $sth = $ppdbh->prepare("select gff3.*, geneid from gff3 left join gff3gene on gffid = gff3.id where id=?");
+				$sth->execute($1);
 				my $row = $sth->fetchrow_hashref();
 				$gff = FPD::GFF3->from_row($row);
 			} else {
 				$gff = FPD::GFF3->from_db(textid => $feature->{name});
 			}
+		### Exonerate alignment features
+		} elsif ($feature->{'type'} eq "exonerate") {
+		### Interpro features
+		} elsif ($feature->{'type'} eq "interpro") {
+		### BLAST alignment features
+		} elsif ($feature->{'type'} eq "blast") {
+		### Fgenesh features
+		} elsif ($feature->{'type'} eq "fgenesh") {
 		}
-
-		### INTERPRO FEATURES
-		### EXONERATE FEATURES
-		### FGENESH FEATURES
-		### BLAST FEATURES
 
 		my $filename = $feature->{type};
 		$filename .= "." . $feature->{track} if ($feature->{track});
